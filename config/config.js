deadc9ee0b5a7e93d996c91707f1ac67cedae112
Hien Han <hienhan0901@gmail.com>
2021-05-11 05:33:32 +0700

    code wallet miner, fix transaction

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
     DIFFICULTY: 2,
     MINE_RATE: 3000,
-    MINING_REWARD: 10
+    MINING_REWARD: 10,
+    INIT_BALANCE: 50
 })
--- test.js
+++ test.js
@@ -1,1 +1,38 @@
-console.log(require('./config/config').MINING_REWARD)
+const BlockChain = require('./lib/blockchain');
+const Blockchain = require('./lib/blockchain');
+const TransactionPool = require('./lib/transactionPool');
+const Wallet = require('./lib/wallet');
+const Transaction = require('./lib/transaction')
+
+let send = new Wallet();
+let receive = new Wallet();
+let third = new Wallet();
+
+let bc = new BlockChain();
+let tp = new TransactionPool();
+
+console.log(`Sender: ${send.getPublicKey()} --- ${send.getPrivate()}`)
+console.log(`Receiver: ${receive.getPublicKey()} --- ${receive.getPrivate()}`)
+console.log(`Third: ${third.getPublicKey()} --- ${third.getPrivate()}`)
+// console.log(bc)
+// console.log(tp)
+console.log('---------------------')
+
+
+send.createTransaction(receive.getPublicKey(), 20, bc, tp)
+receive.createTransaction(send.getPublicKey(), 1, bc, tp)
+third.createTransaction(send.getPublicKey(), 2, bc, tp)
+send.createTransaction(receive.getPublicKey(), 20, bc, tp)
+receive.createTransaction(send.getPublicKey(), 1, bc, tp)
+send.createTransaction(receive.getPublicKey(), 2, bc, tp)
+console.log(tp.transactions)
+
+bc.addBlock(tp.transactions)
+
+console.log(bc.chain[1])
+console.log(send.getBalance(bc))
+console.log(receive.getBalance(bc))
+console.log(third.getBalance(bc))
+
+// console.log(tp.transactions)
+
